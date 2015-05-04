57efc7315a116ec4789adb0b1623e02ecefbd11e
Albino Tonnina <albino.tonnina@workshare.com>
2015-05-04 12:14:37 +0100

    initial

---
+++ test/unit/appSpec.js
@@ -0,0 +1,16 @@
+define(function (require) {
+    'use strict';
+
+    var app = require('app');
+    var Marionette = require('marionette');
+
+    describe('app', function () {
+
+        it('is an instance of marionette.application', function () {
+
+            expect(app instanceof Marionette.Application).toBe(true);
+        });
+
+    });
+
+});
